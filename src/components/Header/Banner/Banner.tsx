eae5c71628e9b8d35b15c8d1f5fdffa259f1ec4a
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-07 16:22:44 +0200

    Flags, images, css
    Add flags in navbar
    Add images in src/images/
    Add css for navbar and banner

--- src/components/Header/Banner/Banner.tsx
+++ src/components/Header/Banner/Banner.tsx
@@ -1,4 +1,7 @@
-import './Banner.css'
+import "./Banner.css";
+
+let googleMapsLink =
+  "https://www.google.com/maps/place/Technical+high+school+%E2%80%9EKiro+Spand%C5%BEov+-+Brko%E2%80%9C/@41.4418811,22.0202293,415m/data=!3m1!1e3!4m6!3m5!1s0x1356650b903daca7:0xabf6272b41d0f0cf!8m2!3d41.4414329!4d22.0199069!16s%2Fg%2F1thjq4kd";
 
 const Banner = () => {
   return (
@@ -12,11 +15,7 @@ const Banner = () => {
         <br />
         со профили од три и четиригодишно траење на образованието.
       </p>
-      <a
-        href="https://www.google.com/maps/place/Technical+high+school+%E2%80%9EKiro+Spand%C5%BEov+-+Brko%E2%80%9C/@41.4418811,22.0202293,415m/data=!3m1!1e3!4m6!3m5!1s0x1356650b903daca7:0xabf6272b41d0f0cf!8m2!3d41.4414329!4d22.0199069!16s%2Fg%2F1thjq4kd"
-        target="_blank"
-        className="map-btn fadeinTop"
-      >
+      <a href={googleMapsLink} target="_blank" className="map-btn fadeinTop">
         Отвори на Google Maps
       </a>
       <section id="section07" className="demo">
--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -1,57 +1,87 @@
+import "./Navbar.css";
+
 const Navbar = () => {
   return (
-  <nav className="navbar navbar-expand-lg py-4 navbar-dark">
-  <div className="container">
-    <div className="logo-lang fadeinLeft">
-      <a href="index.html" className="navbar-brand"
-      ><img src="../../../assets/logo.png" alt=""
-      /></a>
-      <li className="flags"><div className="flag-container">
-        <div className="langSelect">
-          <img className="flag" src="img/Flag_of_Macedonia.png" alt="Makedonski" draggable="false"/>
+    <nav className="navbar navbar-expand-lg py-4 navbar-dark">
+      <div className="container">
+        <div className="logo-lang fadeinLeft">
+          <a href="index.html" className="navbar-brand">
+            <img src="src/images/logo.png" alt="Logo" />
+          </a>
+          <li className="flags">
+            <div className="flag-container">
+              <div className="langSelect">
+                <img
+                  className="flag"
+                  src="src/images/macedonianFlag.png"
+                  alt="Makedonski"
+                  draggable="false"
+                />
+              </div>
+              <div className="langSelect">
+                <img
+                  className="flag"
+                  src="src\images\englishFlag.png"
+                  alt="English"
+                  draggable="false"
+                />
+              </div>
+              <div className="langSelect">
+                <img
+                  className="flag"
+                  src="src/images/albanianFlag.png"
+                  alt="Albanski"
+                  draggable="false"
+                />
+              </div>
+            </div>
+          </li>
         </div>
-        <div className="langSelect">
-          <img className="flag" src="img/Flag_of_the_United_Kingdom.png" alt="Angliski bre!" draggable="false"/>
-        </div>
-        <div className="langSelect">
-          <img className="flag" src="img/Flag_of_Albania.png" alt="Albanski" draggable="false"/>
+        <button
+          className="navbar-toggler md-auto hamburger-menu "
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target=".navmenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navmenu collapse navbar-collapse fadeinRight">
+          <ul className="navbar-nav ms-auto py-2 ">
+            <li className="nav-item">
+              <a href="index.html" className="nav-link">
+                Почетна
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="news.html" className="nav-link">
+                Новости
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="gallery.html" className="nav-link">
+                Галерија
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="struki.html" className="nav-link">
+                Струки
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="aboutus.html" className="nav-link">
+                За Нас
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="contact.html" className="nav-link">
+                Контакт
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      </li>
-      
-    </div>
-    <button
-    className="navbar-toggler md-auto hamburger-menu "
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target=".navmenu"
-    >
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="navmenu collapse navbar-collapse fadeinRight">
-    <ul className="navbar-nav ms-auto py-2 ">
-      <li className="nav-item">
-        <a href="index.html" className="nav-link">Почетна</a>
-      </li>
-      <li className="nav-item">
-        <a href="news.html" className="nav-link">Новости</a>
-      </li>
-      <li className="nav-item">
-        <a href="gallery.html" className="nav-link">Галерија</a>
-      </li>
-      <li className="nav-item">
-        <a href="struki.html" className="nav-link">Струки</a>
-      </li>
-      <li className="nav-item">
-        <a href="aboutus.html" className="nav-link">За Нас</a>
-      </li>
-      <li className="nav-item">
-        <a href="contact.html" className="nav-link">Контакт</a>
-      </li>
-    </ul>
-  </div>
-</div>
-</nav>)
+    </nav>
+  );
 };
 
 export default Navbar;

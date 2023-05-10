af3703574861968ba482a71d8fc734782ec8cf7b
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-10 19:53:41 +0200

    Added functionality to gallery slider onClick

--- src/pages/Gallery/Natprevari/Natprevari.tsx
+++ src/pages/Gallery/Natprevari/Natprevari.tsx
@@ -8,6 +8,10 @@ const images = [
   "/Gallery/Natprevari/3.jpg",
   "/Gallery/Natprevari/4.jpg",
   "/Gallery/Natprevari/5.jpg",
+  "/Gallery/Natprevari/6.jpg",
+  "/Gallery/Natprevari/7.jpg",
+  "/Gallery/Natprevari/8.jpg",
+  "/Gallery/Natprevari/9.jpg",
 ];
 
 const Natprevari = () => {
@@ -15,7 +19,18 @@ const Natprevari = () => {
 
   const viewImage = (img: string, i: number) => {
     setData({ img, i });
-    console.log(img);
+  };
+  const imgAction = (action) => {
+    let i = data.i;
+    if (action === "next-img") {
+      setData({ img: images[i + 1], i: i + 1 });
+    }
+    if (action === "previous-img") {
+      setData({ img: images[i - 1], i: i - 1 });
+    }
+    if (!action) {
+      setData({ img: "", i: 0 });
+    }
   };
 
   return (
@@ -27,12 +42,31 @@ const Natprevari = () => {
             height: "100vh",
             background: "black",
             position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             overflow: "hidden",
+            zIndex: 100,
           }}
         >
+          <button
+            onClick={() => imgAction("")}
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              border: "none",
+              padding: "10px",
+              background: "gray",
+            }}
+          >
+            X
+          </button>
+          <button onClick={() => imgAction("previous-img")}>Previous</button>
           <img
             src={data.img}
             style={{
@@ -41,6 +75,7 @@ const Natprevari = () => {
               maxHeight: "90%",
             }}
           />
+          <button onClick={() => imgAction("next-img")}>Next</button>
         </div>
       )}
       <div style={{ padding: "10px" }}>

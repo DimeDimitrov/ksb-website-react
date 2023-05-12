f34b24275090099a53673b9f9c04c7ff8f0b9369
JovannM <fr0zen1221221@gmail.com>
2023-05-12 14:08:12 +0200

    Fixed nav bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,38 +5,74 @@ const Navbar = () => {
     <div className="container no-gutters">
       <nav className="navbar navbar-expand-lg py-4 navbar-dark">
         <div className="container">
-          <div className="logo-lang fadeinLeft">
-            <a href="home" className="navbar-brand">
-              <img src="/logo.png" alt="Logo" />
-            </a>
-            <li className="flags">
-              <div className="flag-container">
-                <div className="langSelect">
-                  <img
-                    className="flag"
-                    src="/Flags/macedonianFlag.png"
-                    alt="Makedonski"
-                    draggable="false"
-                  />
+          <div className="wrap-element">
+            <div className="logo-lang fadeinLeft">
+              <a href="home" className="navbar-brand">
+                <img src="/logo.png" alt="Logo" />
+              </a>
+              <li className="flags">
+                <div className="flag-container">
+                  <div className="langSelect">
+                    <img
+                      className="flag"
+                      src="/Flags/macedonianFlag.png"
+                      alt="Makedonski"
+                      draggable="false"
+                      />
+                  </div>
+                  <div className="langSelect">
+                    <img
+                      className="flag"
+                      src="/Flags/englishFlag.png"
+                      alt="English"
+                      draggable="false"
+                      />
+                  </div>
+                  <div className="langSelect">
+                    <img
+                      className="flag"
+                      src="/Flags/albanianFlag.png"
+                      alt="Albanski"
+                      draggable="false"
+                      />
+                  </div>
                 </div>
-                <div className="langSelect">
-                  <img
-                    className="flag"
-                    src="/Flags/englishFlag.png"
-                    alt="English"
-                    draggable="false"
-                  />
-                </div>
-                <div className="langSelect">
-                  <img
-                    className="flag"
-                    src="/Flags/albanianFlag.png"
-                    alt="Albanski"
-                    draggable="false"
-                  />
-                </div>
-              </div>
-            </li>
+              </li>
+            </div>
+            <div className="navmenu collapse navbar-collapse fadeinRight">
+              <ul className="navbar-nav ms-auto py-2 ">
+                <li className="nav-item">
+                  <a href="home" className="nav-link">
+                    Почетна
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="news" className="nav-link">
+                    Новости
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="gallery" className="nav-link">
+                    Галерија
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="struki" className="nav-link">
+                    Струки
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="about" className="nav-link">
+                    За Нас
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="contact" className="nav-link">
+                    Контакт
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <button
             className="navbar-toggler md-auto hamburger-menu "
@@ -46,40 +82,6 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="navmenu collapse navbar-collapse fadeinRight">
-            <ul className="navbar-nav ms-auto py-2 ">
-              <li className="nav-item">
-                <a href="home" className="nav-link">
-                  Почетна
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="news" className="nav-link">
-                  Новости
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="gallery" className="nav-link">
-                  Галерија
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="struki" className="nav-link">
-                  Струки
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="about" className="nav-link">
-                  За Нас
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="contact" className="nav-link">
-                  Контакт
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     </div>

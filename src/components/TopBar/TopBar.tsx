208d6f0304a88875fb61577c4e139d2ef30a2327
JovannM <fr0zen1221221@gmail.com>
2023-05-12 14:15:32 +0200

    Added banner

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,11 @@
+import "./TopBar.css";
+
+const TopBar = () => {
+    return (
+        <div className="wrap-Background">
+            <div className="top-bar"></div>
+        </div>
+    );
+};
+
+export default TopBar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,10 +4,12 @@ import Footer from "../../components/Footer/Footer";
 import Header from "./Header/Header";
 import News from "./News/News";
 import Struki from "./Struki/Struki";
+import TopBar from "../../components/TopBar/TopBar";
 
 const Home = () => {
   return (
     <>
+      <TopBar />
       <Header />
       <Struki />
       <News />

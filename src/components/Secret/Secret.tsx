0353fda0bb2a19ecd8f4978ca707a501b7c8c1fc
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-15 00:53:01 +0200

    Added REALNOST

--- src/components/Secret/Secret.tsx
+++ src/components/Secret/Secret.tsx
@@ -1,5 +1,12 @@
+import realnost from "./РЕАЛНОСТ.gif";
+
 const Secret = () => {
-  return <div>https://www.youtube.com/watch?v=p9LYt36FTLE</div>;
+  return (
+    <>
+      <div>https://www.youtube.com/watch?v=p9LYt36FTLE</div>
+      <img src={realnost} alt="" />
+    </>
+  );
 };
 
 export default Secret;

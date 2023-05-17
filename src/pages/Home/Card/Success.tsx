c17cf82f577100f81f8656a946f33054910b4981
Stefchee <81715261+Stefchee@users.noreply.github.com>
2023-05-17 21:54:16 +0200

    Made Test Success Card

---
+++ src/pages/Home/Card/Success.tsx
@@ -0,0 +1,23 @@
+import "./Card1.css";
+
+const Card = () => {
+  return (
+    <div className="card1-main">
+      <div className="card1">
+        <div className="card-body1">
+          <div className="CImg"></div>
+          <h1>Title</h1>
+          <div className="dropdown-content1">
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              nesciunt minus neque accusamus iure nobis, blanditiis natus
+              tempore? Facere earum qui aliquam sit quam?
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import Struki from "./Struki/Struki";
 import Header from "./Header/Header";
 import Liner from "./Liner/Liner";
 import Stories from "./About/About";
-import Card from "./Card/Card";
+import Card from "./Card/Success";
 
 const Home = () => {
   return (
@@ -16,12 +16,7 @@ const Home = () => {
       <Liner />
       <News numPosts={3} />
       <Stories />
-      <Card
-        imageSrc="/News/1.jpg"
-        title="Od KSB do Holywood"
-        description="Od ksb stigna do holywood, wow!"
-        story="Lorem ipsum dolor sit amet amongus Lorem ipsum dolor sit amet amongus Lorem ipsum dolor sit amet amongus "
-      />
+      <Card />
       <br />
       <br />
       <br />

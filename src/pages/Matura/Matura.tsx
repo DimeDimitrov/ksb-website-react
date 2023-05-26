4bd1e056958c54c28cc6ec8ffe66106fe8a8d26b
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-26 17:04:58 +0200

    Add styling to Matura

--- src/pages/Matura/Matura.tsx
+++ src/pages/Matura/Matura.tsx
@@ -1,3 +1,4 @@
+import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Matura.css";
@@ -6,7 +7,10 @@ import MaturaMain from "./MaturaMain/MaturaMain";
 const Matura = () => {
   return (
     <>
-      <Navbar />
+      <Banner img="/Banners/main.webp" text="Матура">
+        <Navbar />
+      </Banner>
+
       <MaturaMain />
       <Footer />
     </>
--- src/pages/Matura/MaturaMain/MaturaMain.tsx
+++ src/pages/Matura/MaturaMain/MaturaMain.tsx
@@ -2,17 +2,18 @@ import "./MaturaMain.css";
 
 const MaturaMain = () => {
   return (
-    <>
+    <div className="matura-wrapper">
+      <h1>Овде може да ги најдете материјалите за матурската</h1>
       <p>
         <a href="/Matura/матура-прашања-Електрични-мрежи.doc">
-          матура прашања Електрични мрежи
+          Матура прашања Електрични мрежи
         </a>
         <br />
-        <a href="/Matura/test-eau-dr-matura.pdf">
+        <a href="/Matura/test-eau-dr-matura.pdf" target="_blank">
           Тест електрични апарати и уреди
         </a>
         <br />
-        <a href="/Matura/матура-прашања.pdf">
+        <a href="/Matura/матура-прашања.pdf" target="_blank">
           Прашања по електрични апарати и уреди
         </a>
         <br />
@@ -20,23 +21,30 @@ const MaturaMain = () => {
           Дигитални, микроконтролери и микропроцесори
         </a>
         <br />
-        <a href="/Matura/KUM-prashanja-priprema.pdf">KUM-prashanja-priprema</a>
+        <a href="/Matura/KUM-prashanja-priprema.pdf" target="_blank">
+          KUM-prashanja-priprema
+        </a>
         <br />
-        <a href="/Matura/KUM-Test-prashanja-mix.pdf">KUM-Test-prashanja-mix</a>
+        <a href="/Matura/KUM-Test-prashanja-mix.pdf" target="_blank">
+          KUM-Test-prashanja-mix
+        </a>
         <br />
         <a href="/Matura/Електроника-прашања-за-интерен-испит.docx">
-          Електроника-прашања за интерен испит
+          Електроника прашања за интерен испит
         </a>
         <br />
         <a href="/Matura/60-прашања-компјутерско-управување-3-та.docx">
           60 прашања компјутерско управување 3 та
         </a>
         <br />
-        <a href="/Matura/Матурски-прашања-за-Мотори-и-моторни-возила-2023.pdf">
+        <a
+          href="/Matura/Матурски-прашања-за-Мотори-и-моторни-возила-2023.pdf"
+          target="_blank"
+        >
           Матурски прашања за Мотори и моторни возила-2023
         </a>
       </p>
-    </>
+    </div>
   );
 };
 

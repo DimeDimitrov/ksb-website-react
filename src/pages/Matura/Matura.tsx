5fa29f25c9f47cb07d20b131522c55efd4455050
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-26 16:55:59 +0200

    Add MaturaMain contents

--- src/pages/Matura/Matura.tsx
+++ src/pages/Matura/Matura.tsx
@@ -1,11 +1,13 @@
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Matura.css";
+import MaturaMain from "./MaturaMain/MaturaMain";
 
 const Matura = () => {
   return (
     <>
       <Navbar />
+      <MaturaMain />
       <Footer />
     </>
   );
--- src/pages/Matura/MaturaMain/MaturaMain.tsx
+++ src/pages/Matura/MaturaMain/MaturaMain.tsx
@@ -4,23 +4,37 @@ const MaturaMain = () => {
   return (
     <>
       <p>
-        <a href="">матура прашања Електрични мрежи</a>
+        <a href="/Matura/матура-прашања-Електрични-мрежи.doc">
+          матура прашања Електрични мрежи
+        </a>
         <br />
-        <a href="">Тест електрични апарати и уреди</a>
+        <a href="/Matura/test-eau-dr-matura.pdf">
+          Тест електрични апарати и уреди
+        </a>
         <br />
-        <a href="">Прашања по електрични апарати и уреди</a>
+        <a href="/Matura/матура-прашања.pdf">
+          Прашања по електрични апарати и уреди
+        </a>
         <br />
-        <a href="">Дигитални, микроконтролери и микропроцесори</a>
+        <a href="/Matura/Digitalna-elektronika-i-mikroprocesor.docx">
+          Дигитални, микроконтролери и микропроцесори
+        </a>
         <br />
-        <a href="">KUM-prashanja-priprema</a>
+        <a href="/Matura/KUM-prashanja-priprema.pdf">KUM-prashanja-priprema</a>
         <br />
-        <a href="">KUM-Test-prashanja-mix</a>
+        <a href="/Matura/KUM-Test-prashanja-mix.pdf">KUM-Test-prashanja-mix</a>
         <br />
-        <a href="">Електроника-прашања за интерен испит</a>
+        <a href="/Matura/Електроника-прашања-за-интерен-испит.docx">
+          Електроника-прашања за интерен испит
+        </a>
         <br />
-        <a href="">60 прашања компјутерско управување 3 та</a>
+        <a href="/Matura/60-прашања-компјутерско-управување-3-та.docx">
+          60 прашања компјутерско управување 3 та
+        </a>
         <br />
-        <a href="">Матурски прашања за Мотори и моторни возила-2023</a>
+        <a href="/Matura/Матурски-прашања-за-Мотори-и-моторни-возила-2023.pdf">
+          Матурски прашања за Мотори и моторни возила-2023
+        </a>
       </p>
     </>
   );

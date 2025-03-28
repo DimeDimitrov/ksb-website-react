61b8c373713636beeaaf0e4d45858f82a7136420
DimeDimitrov <dimedimitrov2005@gmail.com>
2025-03-28 12:40:16 +0100

    Dodaj pozar i stasuvanje

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import News from "./News/News";
 import Struki from "./Struki/Struki";
 import Header from "./Header/Header";
 import Liner from "./Liner/Liner";
-import Stories from './Stories/Stories'
+import Stories from "./Stories/Stories";
 
 const Home = () => {
   return (
@@ -13,10 +13,22 @@ const Home = () => {
       <Header />
       <Struki />
       <Liner />
+      <div className="zastita">
+        <h2>Пожар и спасување</h2>
+        <a href="/elaboratzapokaznavezba.pdf" target="_blank">
+          Елаборат за изведба на показна вежба за заштита од пожар
+        </a>
+        <br />
+        <a href="/planzazastitaispasuvanje.pdf" target="_blank">
+          План за заштита и спасување од природни непогоди и други несреќи
+        </a>
+      </div>
       <News numPosts={3} />
-      <br /><br />
+      <br />
+      <br />
       <Stories />
-      <br /><br />
+      <br />
+      <br />
       <Footer />
     </>
   );
